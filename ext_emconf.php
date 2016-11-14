6a37f23d91f8db6ec832710c6f68f6e40682ad09
ksjogo <jogo@kronberger-spiele.de>
2016-11-14 13:29:40 +0100

    In the beginning was the deed!

---
+++ ext_emconf.php
@@ -0,0 +1,35 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'dkd semantic eye',
+    'description' => '',
+	'category' => 'misc',
+    'author' => '',
+    'author_email' => '',
+	'author_company' => 'dkd Internet Service GmbH',
+	'shy' => '',
+	'dependencies' => 'cms,extbase,fluid',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+    'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+    'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'extbase' => '',
+			'fluid' => '',
+            'typo3' => '7.6.0-8.99.99'
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+);
